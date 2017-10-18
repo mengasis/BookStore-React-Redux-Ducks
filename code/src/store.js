3b4064b84bb52b5bfe506166f001f1c0ab51c4e0
Manuel Venegas <mengasis@outlook.com>
2017-10-18 19:55:35 -0300

    Persist the State in the LocalStorage

--- code/src/store.js
+++ code/src/store.js
@@ -2,19 +2,30 @@ import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { combineReducers } from 'redux-immutable'
+import { fromJS } from 'immutable'
+
 
 import navigationReducer from './ducks/navigation'
 import productsReducer from './ducks/products'
+import { loadState, saveState } from './utils/localStorage'
 
 const reducers = combineReducers({
 	navigation: navigationReducer,
 	products: productsReducer
 })
 
-const store = createStore(reducers, 
+const persistedState = loadState()
+
+const store = createStore(
+	reducers,
+	fromJS(persistedState),
 	composeWithDevTools(
 		applyMiddleware(thunk)
 	)
 )
+
+store.subscribe(() => {
+	saveState(store.getState())
+})
 
 export default store
